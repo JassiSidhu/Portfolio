fbac7137fa5a5136139dea9dc8f98eba8f2cc849
Jaswinder Sidhu <jassi.kaur667@gmail.com>
2019-10-21 10:05:15 -0400

    Fix footer mobile

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,12 +2,12 @@
   <b-container class="fullWidthContainerFooter my-5">
     <b-container>
       <b-row>
-        <b-col cols="1" offset="5">
+        <b-col cols="3" offset="3" md="1" offset-md="5">
           <a href="https://www.linkedin.com/in/jaswinder-sidhu" target="_blank">
             <b-img src="/img/linkedin_logo.png" fluid />
           </a>
         </b-col>
-        <b-col cols="1">
+        <b-col cols="3" md="1">
           <a href="https://github.com/JassiSidhu" target="_blank">
             <b-img src="/img/GitHub-Mark.png" fluid />
           </a>
